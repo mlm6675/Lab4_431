b0420fb068a5731048b100546f52c121ec2a7035
mlm6675 <mlm6675@psu.edu>
2021-02-18 10:12:01 -0500

    fixed

--- src/TrianglePKG/Triangles.java
+++ src/TrianglePKG/Triangles.java
@@ -3,7 +3,7 @@ package TrianglePKG;
 public class Triangles {
     public String foo(int a, int b, int c)
     {
-        if ((a + b) < c || (a + c) < b || (c + b) < a )
+        if ((a + b) <= c || (a + c) <= b || (c + b) <= a )
         {
             return "EXCEPTION";
         }
